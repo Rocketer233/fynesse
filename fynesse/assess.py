01296c9246a4b61dfd2551bb854fce7d79b3d322
Yuang Chen <71658610+Rocketer233@users.noreply.github.com>
2023-11-30 23:14:08 +0000

    Update assess.py

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,6 +1,10 @@
-from .config import *
+from typing import Dict
 
+from .config import *
 from . import access
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
+import osmnx as ox
 
 """These are the types of import we might expect in this file
 import pandas
@@ -13,19 +17,48 @@ import sklearn.feature_extraction"""
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
 
 
-def data():
-    """Load the data from access and ensure missing values are correctly encoded as well as indices correct, column names informative, date and times correctly formatted. Return a structured data structure such as a data frame."""
-    df = access.data()
-    raise NotImplementedError
+def labelled(df, latitude, longitude, box_width=0.01, box_height=0.01):
+    north = latitude + box_height / 2
+    south = latitude - box_height / 2
+    west = longitude - box_width / 2
+    east = longitude + box_width / 2
+    neighbourhood = df[(df["latitude"] >= south) & (df["latitude"] <= north)
+                       & (df["longitude"] >= west) & (df["longitude"] <= east)]
+    return neighbourhood
 
-def query(data):
-    """Request user input for some aspect of the data."""
-    raise NotImplementedError
 
-def view(data):
-    """Provide a view of the data that allows the user to verify some aspect of its quality."""
-    raise NotImplementedError
+# Retrieve POIs
+default_tags: dict[str, bool] = {"amenity": True,
+                                 "buildings": True,
+                                 "historic": True,
+                                 "leisure": True,
+                                 "shop": True,
+                                 "tourism": True}
 
-def labelled(data):
-    """Provide a labelled set of data ready for supervised learning."""
-    raise NotImplementedError
+
+def pois(latitude, longitude, tags=default_tags, box_width=0.02, box_height=0.02):
+    # return the number of points of interests in the bounding box.
+    # default bounding box size 2km
+    north = latitude + box_height / 2
+    south = latitude - box_height / 2
+    west = longitude - box_width / 2
+    east = longitude + box_width / 2
+    pois = ox.features_from_bbox(north, south, east, west, tags)
+    return len(pois)
+
+
+def compute_avg(df, groupby, trim=None):
+    # Given a dataframe, a column `groupby`, this function groups the dataframe by `groupby`
+    # computes the average house prices within each group and plots the graph.
+    # If trim is True, we trim outliers whose values are larger than `trim` to `trim`.
+    grouping = df.groupby(groupby)['price'].mean().reset_index()
+    if trim is not None:
+        grouping['price'] = grouping['price'].apply(lambda x: min(x, trim))
+    plt.figure(figsize=(10, 6))
+    plt.bar(grouping[groupby], grouping['price'])
+    plt.xlabel(groupby)
+    plt.ylabel('Average Price')
+    plt.title(f'Average House Prices by {groupby.capitalize()}')
+    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+    plt.tight_layout()
+    plt.show()
