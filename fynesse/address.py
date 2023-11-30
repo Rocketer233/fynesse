276b742fbe8c2a8e34c22d74ca6346dc53860ad7
Yuang Chen <71658610+Rocketer233@users.noreply.github.com>
2023-11-30 23:13:38 +0000

    Update address.py

--- fynesse/address.py
+++ fynesse/address.py
@@ -1,18 +1,40 @@
 # This file contains code for suporting addressing questions in the data
 
-"""# Here are some of the imports we might expect 
-import sklearn.model_selection  as ms
-import sklearn.linear_model as lm
-import sklearn.svm as svm
-import sklearn.naive_bayes as naive_bayes
-import sklearn.tree as tree
-
-import GPy
-import torch
-import tensorflow as tf
-
-# Or if it's a statistical analysis
-import scipy.stats"""
-
 """Address a particular question that arises from the data"""
+
+from datetime import datetime
+import numpy as np
+from . import assess
+
+
+def get_times(df):
+    times = []
+    for _, row in df.iterrows():
+        if type(row["date_of_transfer"]) == str:
+            year = datetime.strptime(row["date_of_transfer"], "%Y-%m-%d").year
+        else:
+            year = row["date_of_transfer"].year
+        times.append(year)
+    return times
+
+
+default_mapping = {'D':4.5, 'F':3.1, 'O':12, 'S':2.8, 'T':2.6}
+
+
+def get_property_types(df, mapping = default_mapping):
+    return [mapping[row["property_type"]] for _, row in df.iterrows()]
+
+
+def get_pois(df):
+    return [assess.pois(row["latitude"], row["longitude"]) for _, row in df.iterrows()]
+
+
+def get_design_matrix(df):
+    days = np.array(get_times(df))
+    property_types = np.array(get_property_types(df))
+    pois = np.array(get_pois(df))
+    design = np.concatenate((days.reshape(-1, 1), property_types.reshape(-1, 1),
+                            pois.reshape(-1, 1)), axis=1)
+    return design
+
 
